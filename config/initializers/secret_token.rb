5a5f461b9f4cbcd737526bbef110314f5334dd84
Wang Yan <wangyan87@gmail.com>
2012-11-16 11:48:47 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workshop::Application.config.secret_token = '1bb08167befb6be02dccff0e3766bbacdade24e5bc162d05e57066b6056ff3f71adbc43c083bd046b6fe49aafcfcc6779788eb7a67cd11fd4b1bf6d6c3456460'
